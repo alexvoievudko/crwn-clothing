53645d75a88fcc98dd7717cca7b021b3f9654e2e
Alex Voievudko <alex.voievudko@gmail.com>
2022-01-11 11:31:39 +0100

    implemented firebase utils, included ability to store authenticated users into dfirestore db

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.components";
 
 import "./signin-and-signup.styles.scss";
 
@@ -8,6 +9,7 @@ const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
